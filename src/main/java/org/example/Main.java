b6301031650df80037a4473ea728524e0bfa7c05
ursulakohler <kohlerursula22@gmail.com>
2024-07-12 16:26:59 +0200

    add hello world

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,19 @@
 package org.example;
+//Schritt 1: Erstellen Sie ein neues Projekt.
+//Schritt 2: Veröffentlichen Sie dieses Projekt auf GitHub.
+//Schritt 3: Erstellen Sie einen neuen Funktionszweig mit einem aussagekräftigen Namen.
+//Schritt 4: Nehmen Sie Änderungen in Ihrem Funktionszweig vor, übertragen Sie sie und pushen Sie sie.
+//Schritt 5: Wechseln Sie zurück zum Hauptzweig (z.B. "main" oder "master") und nehmen Sie neue Änderungen vor, übertragen Sie sie und pushen Sie sie.
+//Schritt 6: Führen Sie die Änderungen aus dem Hauptzweig lokal in Ihrem Funktionszweig zusammen.
+//Schritt 7: Lösen Sie alle Konflikte während des Zusammenführens auf.
+//Schritt 8: Verschieben Sie Ihren Funktionszweig auf GitHub.
+//Schritt 9: Erstellen Sie einen Pull-Request für Ihren Funktionszweig und lassen Sie ihn von Ihren Kollegen überprüfen.
+//Schritt 10: Nach der Genehmigung führen Sie die Zusammenführung durch und löschen den Funktionszweig.
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Hello World!");
     }
 }
