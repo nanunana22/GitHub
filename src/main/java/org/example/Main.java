b6af8c0325cab500a326015ff84d18a2aea6b480
ursulakohler <kohlerursula22@gmail.com>
2024-07-12 16:32:32 +0200

    add Hello World

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,5 +15,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("My second Feature Branch");
     }
 }
